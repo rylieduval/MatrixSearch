b688a83d1bf35ac136636acb7ed7821aec2ffec3
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-09-20 17:30:31 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -4,11 +4,11 @@ public class Matrix
 {
     public static void main(String[] args)
     {
-        Matrix newArray = new Matrix();
         int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        int[] ans = MatrixSearch(arr, 8);
-        
-        System.out.println(Arrays.toString(ans));
+        //create an array full of the correct matrix coordinates
+        int[] answer = MatrixSearch(arr, 8);
+        //print out the answer as a string
+        System.out.println(Arrays.toString(answer));
     }
     
     static int[] MatrixSearch(int[][] arr, int key)
@@ -16,10 +16,12 @@ public class Matrix
         int row = arr.length;
         int col = arr[0].length;
         int l = 0;
+        //determine the number of spots in the matrix
         int h = row * col - 1;
  
         while (l <= h) 
         {
+            //find the middle of the matrix
             int mid = l + (h - l) / 2;
  
             int tC = mid % col;
@@ -28,13 +30,16 @@ public class Matrix
             
             if (val == key)
             {
+                //return the coordinates of the matrix in which the key is found
                 return new int[] { tR, tC };
             }
- 
+             // check to see if the current value is less than the target number
             if (val < key)
             {
                 l = mid + 1;
             }
+             // check to see if the current value is higher than the target number
+
             else
             {
                 h = mid - 1;
